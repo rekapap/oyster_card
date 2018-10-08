e151f342cb75734908ff9d1b80533286ab334310
Reka <rekapap28@gmail.com>
2018-10-08 14:44:48 +0100

    adds top_up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(amount)
+    @balance += amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it 'should top_up' do
+    subject.top_up(5)
+    expect(subject.balance).to eq 5
+  end
+
 end
